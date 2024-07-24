343d40a7523983d4cde11e8688e17f88af91995f
Enigmatic Aura <60955413+adicahyadir@users.noreply.github.com>
2024-07-24 16:47:51 +0700

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/", 
+    },
+  },
+};
